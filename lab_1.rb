42cb010129b08f0dc17f3d906f042b929f74a539
Kirill <96747698+KoriganGH@users.noreply.github.com>
2023-02-14 16:22:42 +0300

    Update lab_1.rb

--- lab_1.rb
+++ lab_1.rb
@@ -1,1 +1,14 @@
 puts "Hello World"
+
+user = ARGV[0]
+puts "#{user}, какой твой любимый язык программирования?"
+case STDIN.gets.chomp
+when "Python" 
+  puts "Мда..."
+when "Ruby" 
+  puts "Ну ты и подлиза!"
+when "F#" 
+  puts "Не верю"
+else 
+  puts "Я такой не знаю"
+end
