910ea8e0a17ea28ec52fd04d623720d81fcf5224
Kirill <96747698+KoriganGH@users.noreply.github.com>
2023-02-21 15:15:53 +0300

    Update task_2.rb

--- lab_1/task_2.rb
+++ lab_1/task_2.rb
@@ -1,3 +1,21 @@
+#1
+def is_prime(n)
+  return false if n < 2
+  (2..n-1).none? { |i| n % i == 0 }
+end
+
+def sum_of_non_prime_divisors(n)
+  sum = 0
+  (2..n/2).each do |i|
+    if n % i == 0 && !is_prime(i)
+      sum += i
+    end
+  end
+  sum
+end
+
+
+#2
 def count_digits_less_than_3(n)
   count = 0
   while n > 0
