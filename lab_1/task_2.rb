14677312ffc1f3813f8563387154545c8e8b3865
Kirill <96747698+KoriganGH@users.noreply.github.com>
2023-02-21 15:18:10 +0300

    Update task_2.rb

--- lab_1/task_2.rb
+++ lab_1/task_2.rb
@@ -27,3 +27,36 @@ def count_digits_less_than_3(n)
   end
   count
 end
+
+
+#3
+def is_coprime(a, b)
+  (2..[a, b].min).none? { |i| a % i == 0 && b % i == 0 }
+end
+
+def is_prime_digit(digit)
+  [2, 3, 5, 7].include?(digit)
+end
+
+def sum_of_prime_digits(n)
+  sum = 0
+  while n > 0
+    digit = n % 10
+    if is_prime_digit(digit)
+      sum += digit
+    end
+    n /= 10
+  end
+  sum
+end
+
+def count_numbers(n)
+  prime_digits_sum = sum_of_prime_digits(n)
+  count = 0
+  (1..n-1).each do |i|
+    if n % i != 0 && !is_coprime(i, n) && is_coprime(i, prime_digits_sum)
+      count += 1
+    end
+  end
+  count
+end
