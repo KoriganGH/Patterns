13bd893d487997343f575ab98974bbaa6946b319
Kirill <96747698+KoriganGH@users.noreply.github.com>
2023-03-21 15:53:00 +0300

    Update student.rb

--- lab_2/student.rb
+++ lab_2/student.rb
@@ -1,4 +1,6 @@
 class Student
+  attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :git
+
   def initialize(last_name, first_name, middle_name = nil)
     @id = nil
     @last_name = last_name
@@ -8,70 +10,6 @@ class Student
     @telegram = nil
     @email = nil
     @git = nil
-  end
-
-  def id
-    @id
-  end
-
-  def id=(id)
-    @id = id
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def last_name=(last_name)
-    @last_name = last_name
-  end
-
-  def first_name
-    @first_name
-  end
-
-  def first_name=(first_name)
-    @first_name = first_name
-  end
-
-  def middle_name
-    @middle_name
-  end
-
-  def middle_name=(middle_name)
-    @middle_name = middle_name
-  end
-
-  def phone
-    @phone
-  end
-
-  def phone=(phone)
-    @phone = phone
-  end
-
-  def telegram
-    @telegram
-  end
-
-  def telegram=(telegram)
-    @telegram = telegram
-  end
-
-  def email
-    @email
-  end
-
-  def email=(email)
-    @email = email
-  end
-
-  def git
-    @git
-  end
-
-  def git=(git)
-    @git = git
   end
 
   def to_s
