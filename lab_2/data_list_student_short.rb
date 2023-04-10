eeb303ffdc690f688ee15da67ab7a9dc56f569b9
Kirill <96747698+KoriganGH@users.noreply.github.com>
2023-04-10 23:26:19 +0300

    3.5

---
+++ lab_2/data_list_student_short.rb
@@ -0,0 +1,24 @@
+require_relative 'data_list'
+
+class DataListStudentShort < DataList
+  public_class_method :new
+
+  def column_names
+    %w[git contact last_name_and_initials]
+  end
+
+  def data_table
+    result = []
+    counter = 0
+    objects.each do |obj|
+      row = []
+      row << counter
+      row << obj.git
+      row << obj.contact
+      row << obj.last_name_and_initials
+      result << row
+      counter += 1
+    end
+    DataTable.new(result)
+  end
+end
