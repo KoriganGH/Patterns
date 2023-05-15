47dd141eb2b69c704a94d56ce598a98ab4edf2aa
Kirill <96747698+KoriganGH@users.noreply.github.com>
2023-05-16 00:53:40 +0300

    3.1

---
+++ StudentLab/dml.sql
@@ -0,0 +1,4 @@
+insert into students(last_name, first_name, second_name, phone, telegram, email, git)
+values ('Иванов', 'Иван', 'Иванович', '89282604886', 'd3sss', NULL, NULL),
+       ('Петров', 'Петр', 'Алексеевич', NULL, NULL, 'petya2@gmail.com', NULL),
+       ('Кумков', 'Иван', 'Иванович', '89282604777', '@kumok', 'kks13@mail.ru', NULL );
