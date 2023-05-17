54fa0e86a8bbe77363557c500c89aa7796a5ea00
Kirill <96747698+KoriganGH@users.noreply.github.com>
2023-05-17 15:05:45 +0300

    start migration

--- StudentLab/dml.sql
+++
@@ -1,4 +0,0 @@
-insert into students(last_name, first_name, second_name, phone, telegram, email, git)
-values ('Иванов', 'Иван', 'Иванович', '89282604886', 'd3sss', NULL, NULL),
-       ('Петров', 'Петр', 'Алексеевич', NULL, NULL, 'petya2@gmail.com', NULL),
-       ('Кумков', 'Иван', 'Иванович', '89282604777', '@kumok', 'kks13@mail.ru', NULL );
