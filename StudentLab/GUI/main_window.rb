8bd62f2c933b3d9cc1b9acbb9f7ae678cc2e342f
Kirill <96747698+KoriganGH@users.noreply.github.com>
2023-05-19 23:00:38 +0300

    3.3 (3 empty tabs)

---
+++ StudentLab/GUI/main_window.rb
@@ -0,0 +1,17 @@
+require 'glimmer-dsl-libui'
+
+class MainWindow
+  include Glimmer
+  def create
+    window('Университет', 600, 200) {
+      tab {
+        tab_item('Студенты') {
+
+        }
+
+        tab_item('Вкладка 2') { }
+        tab_item('Вкладка 3') { }
+      }
+    }
+  end
+end
