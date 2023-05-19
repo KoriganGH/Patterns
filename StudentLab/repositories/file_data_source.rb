158806ceae55d86c97f1aaff37a05d3add10332b
Kirill <96747698+KoriganGH@users.noreply.github.com>
2023-05-19 13:34:35 +0300

    3.2

--- StudentLab/repositories/file_data_source.rb
+++ StudentLab/repositories/file_data_source.rb
@@ -1,28 +1,29 @@
-class SuperStudentsList
-  attr_writer :data_type
+class FileDataSource
+  attr_writer :data_transformer
 
-  def initialize(data_type)
+  def initialize(data_transformer)
     self.students = []
     self.seq_id = 1
-    self.data_type = data_type
+    self.data_transformer = data_transformer
   end
 
   def load_from_file(file_path)
-    hash_list = data_type.str_to_hash_list(File.read(file_path))
+    hash_list = data_transformer.str_to_hash_list(File.read(file_path))
     self.students = hash_list.map { |h| Student.from_hash(h) }
     update_seq_id
   end
 
   def save_to_file(file_path)
     hash_list = students.map(&:to_hash)
-    File.write(file_path, data_type.hash_list_to_str(hash_list))
+    File.write(file_path, data_transformer.hash_list_to_str(hash_list))
   end
 
   def student_by_id(student_id)
     students.detect { |s| s.id == student_id }
   end
 
-  def paginated_short_students(page, count, existing_data_list: nil)
+  # Получить page по счету count элементов (страница начинается с 1)
+  def paginated_short_students(page, count, existing_data_list = nil)
     offset = (page - 1) * count
     slice = students[offset, count].map { |s| StudentShort.from_student(s) }
 
@@ -44,7 +45,7 @@ class SuperStudentsList
   end
 
   def replace_student(student_id, student)
-    idx = student.find_index { |s| s.id == student_id }
+    idx = students.find_index { |s| s.id == student_id }
     students[idx] = student
   end
 
@@ -58,10 +59,11 @@ class SuperStudentsList
 
   private
 
+  # Метод для актуализации seq_id
   def update_seq_id
     self.seq_id = students.max_by(&:id).id + 1
   end
 
-  attr_reader :data_type
+  attr_reader :data_transformer
   attr_accessor :students, :seq_id
 end
