a33510b9fa9dd277870ec2209ea88911b842dd79
Kirill <96747698+KoriganGH@users.noreply.github.com>
2023-05-20 01:46:45 +0300

    3.4 events

--- StudentLab/repositories/containers/data_list.rb
+++ StudentLab/repositories/containers/data_list.rb
@@ -1,10 +1,25 @@
+require './repositories/containers/data_table'
+
 class DataList
   private_class_method :new
-  
+
   attr_writer :objects
-  
+
   def initialize(objects)
     self.objects = objects
+    @listeners = []
+  end
+
+  def add_listener(listener)
+    @listeners << listener
+  end
+
+  def remove_listener(listener)
+    @listeners.delete(listener)
+  end
+
+  def notify
+    @listeners.each { |lst| lst.on_datalist_changed(data_table) }
   end
 
   def select_element(number)
@@ -13,10 +28,6 @@ class DataList
 
   def selected_id
     objects[selected_num].id
-  end
-
-  def column_names
-    raise NotImplementedError('Должен быть реализован в дочернем')
   end
 
   def data_table
@@ -32,10 +43,19 @@ class DataList
     DataTable.new(result)
   end
 
+  def replace_objects(objects)
+    self.objects = objects.dup
+    notify
+  end
+
   protected
 
   def table_fields(_obj)
-    raise NotImplementedError('Должен быть реализован в дочернем')
+    []
+  end
+
+  def column_names
+    []
   end
 
   private
